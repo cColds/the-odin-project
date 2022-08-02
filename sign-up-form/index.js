943a2a0de82ede934006d5e1b5dc7a4e3ec3c5cd
vyachnd <vyacheslav.nodata@yandex.ru>
2022-08-02 11:43:32 +0300

    feat: wrap page content in container

--- sign-up-form/index.js
+++ sign-up-form/index.js
@@ -1,5 +1,18 @@
-const mainElement = document.querySelector('.main .wrapper');
+window.addEventListener('load', () => {
+  const app = document.getElementById('app');
+  const appMain = app.querySelector('.app__main .wrapper');
+  const loginBtn = app.querySelector('#log-in');
+  const signupBtn = app.querySelector('#sign-up');
 
-if (mainElement instanceof HTMLElement) {
-  console.log(mainElement);
-}
+  if (loginBtn instanceof HTMLElement) {
+    loginBtn.addEventListener('click', () => console.log('#log-in'));
+  }
+
+  if (signupBtn instanceof HTMLElement) {
+    signupBtn.addEventListener('click', () => console.log('#sign-up'));
+  }
+
+  if (appMain instanceof HTMLElement) {
+    console.log('.app__main');
+  }
+});
