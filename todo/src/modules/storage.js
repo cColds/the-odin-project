edb92cdb5f4ac1696fade7f4295c2eb83cd1acbf
vyachnd <vyacheslav.nodata@yandex.ru>
2022-09-07 12:48:00 +0300

    feat: implement localstorage save/load

---
+++ todo/src/modules/storage.js
@@ -0,0 +1,12 @@
+export default {
+  save(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+
+  async load(key) {
+    const data = await localStorage.getItem(key);
+    const res = await JSON.parse(data);
+
+    return res;
+  },
+};
