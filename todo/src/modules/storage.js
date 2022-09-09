d1b95762307e48a59134adcc22538cc190858f14
vyachnd <vyacheslav.nodata@yandex.ru>
2022-09-09 20:51:58 +0300

    feat: add local-key for local-storage keys

--- todo/src/modules/storage.js
+++ todo/src/modules/storage.js
@@ -1,10 +1,12 @@
+const LOCAL_KEY = 'vyachnd-todo';
+
 export default {
   save(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(`${LOCAL_KEY}-${key}`, JSON.stringify(data));
   },
 
   async load(key) {
-    const data = await localStorage.getItem(key);
+    const data = await localStorage.getItem(`${LOCAL_KEY}-${key}`);
     const res = await JSON.parse(data);
 
     return res || [];
