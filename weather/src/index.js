fcbc36a19456e092ebf476e51cfebba9e80bdfbc
vyachnd <vyacheslav.nodata@yandex.ru>
2022-09-18 21:25:24 +0300

    feat: init app component

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,2 +1,13 @@
-console.log(process.env.API_WEATHER);
-console.log('init index.js');
+/*
+  Weather icons by Leya Kuznetsova
+
+  Author - https://www.figma.com/@leyakuznetsova
+  Link - https://www.figma.com/community/file/1059229179375580154
+*/
+import './scss/main.scss';
+import App from './components/app/app';
+
+App().render({
+  node: document.body,
+  appendType: 'prepend',
+});
